90a5017d8af0343668dc502fa2f113088ed888a2
kalamam <moshiko.kalamaro@gmail.com>
2022-08-18 22:26:12 +0300

    finished

--- Flower.cpp
+++ Flower.cpp
@@ -4,19 +4,20 @@
 #include <algorithm>
 
 using namespace std;
-
+//a comparison method used to sort the flowers by distance.
 bool Flower::comparison(Flower a, Flower b)
 {
 	return (a.getDistance() < b.getDistance());
 }
+//returns true if the str vector contains s.
 bool Flower::contains(vector<string> vec, string s) {
 	for (string x : vec) {
 		if (x.compare(s) == 0) return true;
 	}
 	return false;
 }
+//returns the index of the largest element in the int vector.
 int Flower::getMaxElementIndex(vector<int> vector){
-	//returns the number of which the vector value is the biggest.
 	int maxElementIdx = 0;
 	for (int i = 0; i < vector.size(); i++)
 	{
@@ -36,14 +37,14 @@ vector<string> Flower::getFlowerTypes(vector<Flower> flowerVec){
 	return flowerTypes;
 }
 void Flower::classify(vector<Flower> classifiedVec, int k, double (Flower::*distanceFunction)(Flower)){
-	//this function checks the knn for an unclassified flower and changes its type.
-	//the function also gets the distance function to use while calculating the knn.
+	//this function checks the knn for an this flower and classifies it, we assume this flower is not classified.
+	//the function gets the distance function to use while calculating the knn.
 	for (int i = 0; i < classifiedVec.size(); i++) {
 		(classifiedVec[i].*distanceFunction)(*this);
-	}//we calculate the distance of each flower from the given flower.
+	}//we calculate the distance of each flower from this flower.
 	vector<string> flowerTypes = getFlowerTypes(classifiedVec);
 	sort(classifiedVec.begin(), classifiedVec.end(), comparison);
-	//then we sort the vector according to the distance from the given flower.
+	//then we sort the vector according to the distance from this flower.
 	vector<int> numOfFlowerType(flowerTypes.size());
 	for (int i : numOfFlowerType) i = 0;
 	for (int i = 0; i < k; i++)
@@ -54,12 +55,12 @@ void Flower::classify(vector<Flower> classifiedVec, int k, double (Flower::*dist
 			}
 		}
 	}
-	// now we know that the knn are the first k elements in the vector
-	//so we calculate how many instances there are of each flower and change the flower type to the type with the most instances.
+	// now we know that the knn are the first k elements in the vector so we count
+	// how many instances there are of each type and classify this flower to be of the type with the most instances.
 	this->setType(flowerTypes[getMaxElementIndex(numOfFlowerType)]);
 }
 double Flower::euclideanDistance(Flower other) {
-	//the function calculates the distance between 2 flowers using the euclidean way
+	//the function calculates the euclidean distance between 2 flowers
 	double sqrDistance = 0;
 	for (int i = 0; i < 4; i++) {
 		sqrDistance += (this->attributes[i] - other.attributes[i]) * (this->attributes[i] - other.attributes[i]);
@@ -68,7 +69,7 @@ double Flower::euclideanDistance(Flower other) {
 	return this->distance;
 }
 double Flower::manhattanDistance(Flower other) {
-	//the function calculates the distance between 2 flowers using the manhattan way
+	//the function calculates the manhattan distance between 2 flowers.
 	this->distance = 0;
 	for (int i = 0; i < 4; i++) {
 		this->distance += abs(this->attributes[i] - other.attributes[i]);
@@ -76,7 +77,7 @@ double Flower::manhattanDistance(Flower other) {
 	return this->distance;
 }
 double Flower::chebyshevDistance(Flower other) {
-	//the function calculates the distance between 2 flowers using the chebyshev way
+	//the function calculates the chebyshev distance between 2 flowers.
 	double maxDistance = 0;
 	for (int i = 0; i < 4; i++) {
 		maxDistance = max(abs(this->attributes[i] - other.attributes[i]), maxDistance);
@@ -84,11 +85,10 @@ double Flower::chebyshevDistance(Flower other) {
 	this->distance = maxDistance;
 	return this->distance;
 }
-
+//setters
 void Flower::setType(string type) {
 	this->type = type;
 }
-
 void Flower::setAttribute(double value, int index) {
 	if (index >= sizeof(this->attributes) / sizeof(this->attributes[0])) {
 		return;
--- tcp_client.cpp
+++ tcp_client.cpp
@@ -13,17 +13,22 @@ using namespace std;
 
 
 int main(int argc, char** argv){
+    //if the user did not type enough arguments.
 	if (argc < 3) {
 		cout << "Error, Invalid usage" << endl;
 		exit(1);
 	}
+    // the first argument is the path to the unclassified input file.
     string inputFile = argv[1];
+    // the second argument if the path to create the output file at.
     string outputFile = argv[2];
     ifstream unclassified;
     unclassified.open(inputFile);
     ofstream classifiedOutput;
     classifiedOutput.open(outputFile);
+
     const char* ip_address = "127.0.0.1";
+    //needs to match the client port.
     const int port_no = 5553;
 
     int sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -40,10 +45,11 @@ int main(int argc, char** argv){
     if (connect(sock, (struct sockaddr *) &sin, sizeof(sin)) < 0) {
         perror("error connecting to server");
     }
+    //loop through all the lines of the unclassified file.
     while (unclassified.good()) {  
         string line;
         getline(unclassified, line);
-        if (line.empty()){
+        if (line.empty()){ 
             break;
         }
         int data_len = line.length();
@@ -52,10 +58,11 @@ int main(int argc, char** argv){
             perror("error while sending message");
             break;
         }
-        char buffer[1000];
+        char buffer[100];
         int expected_data_len = sizeof(buffer);
         int read_bytes = recv(sock, buffer, expected_data_len, 0);
-        if (read_bytes == 0) {
+        if (read_bytes == 0){
+            //conection closed.
             break;
         }
         else if (read_bytes < 0) {
@@ -63,11 +70,10 @@ int main(int argc, char** argv){
             break;
         }
         else {
+            //write the flower type to the output file.
             classifiedOutput << buffer << endl;
         }
     }
     close(sock);
-
-
     return 0;
 }
