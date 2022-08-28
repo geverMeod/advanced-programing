399a1469f23aed0afde42b50c5307dbff3cf6f91
kalamam <moshiko.kalamaro@gmail.com>
2022-08-28 17:57:13 +0300

    added Classify Data command

---
+++ ClassifyDataCommand.hpp
@@ -0,0 +1,23 @@
+#include "Command.hpp"
+#include "thread"
+#pragma once
+
+using namespace std;
+class ClassifyDataCommand : Command {
+    public:
+        
+        
+        ClassifyDataCommand(DefultIO* dio, UserData* data) : Command("classify data", dio, data) {}
+
+        void execute() override{
+            if (this->getData()->getUnclassified().empty())
+            {
+                this->getIO()->write("Please upload files first");
+                return;
+            }
+             this->getIO()->write("Started classifying");
+            //  thread classify([this] { this->getData()->classify(); });
+            thread classify(&UserData::classify, this->getData());
+            classify.detach();
+        }
+};
