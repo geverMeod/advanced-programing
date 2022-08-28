8859d73f76fbbcda4a183d06b1d053e93c8d2a1d
kalamam <moshiko.kalamaro@gmail.com>
2022-08-28 17:02:46 +0300

    Added a class that controls the server's side

---
+++ Server.h
@@ -0,0 +1,26 @@
+#include <netinet/in.h>
+#include "string"
+
+class Server
+{
+private:
+    int clientNum = 0;
+    int socketId;
+    int queueSize = 5;
+    struct sockaddr_in readAddr;
+    fd_set readfds;
+    struct timeval timeVal;
+public:
+    Server(in_addr_t ip, in_port_t port);
+
+    int acceptClient();
+        
+    void removeClient();
+
+    void closeServerSock() const;
+
+    ~Server() = default;
+
+    int getClientNum() const;
+};
+
